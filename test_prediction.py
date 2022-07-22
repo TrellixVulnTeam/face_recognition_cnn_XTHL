16b5159aad66ee4f6e3a36dce7934c1f562fc8d2
Im-Rises <quentin-morel88@hotmail.com>
2022-07-22 11:15:59 +0200

    Added test file

--- test_prediction.py
+++ test_prediction.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri June 22 16:15:10 2022
+test_prediction.py python file
+@author: Im-Rises
+"""
+
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
+    """
+    Assert on error class unit test
+    """
     def test_something(self):
+        """
+        Assert on error function
+        :return:
+        """
         self.assertEqual(True, True)  # add assertion here
 
 
